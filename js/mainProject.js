b7689ba645a0c237b37d1a9d43bd80d99e121ba6
Alex <alex.smartens@gmail.com>
2019-05-12 19:40:50 -0400

    Improved code structure

--- js/mainProject.js
+++ js/mainProject.js
@@ -1,59 +1,47 @@
 // Canvas parameters
 var margin = {top: 100, right: 10, bottom: 75, left: 75},
     width = 800 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom
+    height = 600 - margin.top - margin.bottom;
 
-let svg = d3.select("#chart-area")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+// Construct chart
+let canvas = d3.select("#chart-area")
+                .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom),
+    chart = canvas.append("g")
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-let chart = svg.append("g")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-
-// Add chart title
-chart.append("text")
-    .attr("class", "x-axis-label")
-    .attr("x", width/2)
-    .attr("y", -50)
-    .attr("text-anchor", "middle")
-    .attr("font-size", 18)
-
-// Add axes labels
-chart.append("text")
-    .attr("class", "y-axis-label")
-    .attr("x", -height/2)
-    .attr("y", -50)
-    .attr("text-anchor", "middle")
-    .attr("font-size", 18)
-    .attr("transform","rotate(-90)")
-    .text("Life Expectancy (years)")
-
-chart.append("text")
-    .attr("class", "x axis-label")
-    .attr("x", width/2)
-    .attr("y", height + 50)
-    .attr("text-anchor", "middle")
-    .attr("font-size", 18)
-    .text("GDP Per Capita")
-
-// Add current year
-let current_year_text = chart.append("text")
-    .attr("class", "current-year")
-    .attr("x", width)
-    .attr("y", height - 10)
-    .attr("text-anchor", "end")
-    .attr("font-size", 40)
-    .attr("fill", "gray")
-
-// Define scales
+// Scales and legend
 let y = d3.scaleLinear()
-let x = d3.scaleLog()
-let area = d3.scaleLinear()
-let continentColor = d3.scaleOrdinal()
+          .range([height, 0])
+          .nice(),
+    yAxisCall= d3.axisLeft(y),
 
-// Animation time interval
-let timeInterval = 100
+    x = d3.scaleLog()
+          .range([0, width])
+          .base(10),
+    xAxisCall = d3.axisBottom(x)
+        .ticks(3)
+        .tickValues([400, 4000, 40000])
+        .tickFormat(d => "$" + d),
+          
+    area = d3.scaleLinear()
+             .range([70, 3000]),
+
+    continentColor = d3.scaleOrdinal()
+                       .range(d3.schemeCategory10),
+    
+    legend = chart.append("g")
+        .attr("transform", "translate(" + (width - 10) + "," + (height - 125) + ")");
+    
+
+
+// Additional parameters
+let timeInterval = 100,
+    time = 0,
+    interval,
+    data,
+    dataLength;
 
 // Tooltip
 let tip = d3.tip().attr('class', 'd3-tip')
@@ -67,181 +55,190 @@ let tip = d3.tip().attr('class', 'd3-tip')
     });
 chart.call(tip);
 
-let time = 0;
-let interval;
-let dataFiltered;
+// // Add chart title
+// chart.append("text")
+//     .attr("class", "x-axis-label")
+//     .attr("x", width/2)
+//     .attr("y", -50)
+//     .attr("text-anchor", "middle")
+//     .attr("font-size", 18);
 
+// Add axes labels
+chart.append("text")
+    .attr("class", "y-axis-label")
+    .attr("x", -height/2)
+    .attr("y", -50)
+    .attr("text-anchor", "middle")
+    .attr("font-size", 18)
+    .attr("transform","rotate(-90)")
+    .text("Life Expectancy (years)");
+chart.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width/2)
+    .attr("y", height + 50)
+    .attr("text-anchor", "middle")
+    .attr("font-size", 18)
+    .text("GDP Per Capita");
 
-
-
-
-
-// Load and process data
-d3.json("data/data.json").then(function(data){
-    
-    // Filter data
-    dataFiltered = []
-    for (let yearIdx=0; yearIdx < data.length; yearIdx++){
-        let dataCurrentYear = {year: data[yearIdx].year, countries: []}
-        for (let countryIdx=0; countryIdx < data[yearIdx].countries.length; countryIdx++){
-            let d = data[yearIdx].countries[countryIdx]
-            if ( !isNaN(parseFloat(d.life_exp)) && !isNaN(parseFloat(d.income)) && !isNaN(parseFloat(d.population)) ){
-                dataCurrentYear.countries.push(d)
-            } 
-        }
-        dataFiltered.push(dataCurrentYear)
-    }
-
-    initialize(dataFiltered)
-    // Run of the first visualization
-    update(dataFiltered[0])
-    
-})
-
+// Add current year
+let currentYearLabel = chart.append("text")
+    .attr("class", "current-year")
+    .attr("x", width)
+    .attr("y", height - 10)
+    .attr("text-anchor", "end")
+    .attr("font-size", 40)
+    .attr("fill", "gray");
 
 // Button controllers
 $("#play-button")
     .on("click", function(){
         let button = $(this);
-        if (button.text() == "Play"){
+        if (button.text() === "Play"){
             button.text("Pause");
-            interval = setInterval(step, 100);            
-        }
-        else {
+            interval = setInterval(step, timeInterval);            
+        } else {
             button.text("Play");
             clearInterval(interval);
         }
     })
-
 $("#reset-button")
     .on("click", function(){
         time = 0;
-        update(dataFiltered[0]);
+        updateChart(data[0]);
     })
-
 $("#continent-select")
     .on("change", function(){
-        update(dataFiltered[time]);
+        updateChart(data[time]);
+    })
+$("#date-slider")
+    .slider({
+        max: 2014,
+        min: 1800,
+        step: 1,
+        slide: function(event, ui){
+            time = ui.value - 1800;
+            updateChart(data[time]);
+        }
     })
 
-$("#date-slider").slider({
-    max: 2014,
-    min: 1800,
-    step: 1,
-    slide: function(event, ui){
-        time = ui.value - 1800;
-        update(dataFiltered[time]);
-    }
-})
+function drawLegend(continents){
+    continents.forEach(function(continent, i){
+        let row = legend.append("g")
+            .attr("transform", "translate(0, " + (i * 20) + ")");
 
-function initialize(dataFiltered){
-    // Compute scales
-    y
-        .domain([0, 90])
-        // .domain([0, d3.max(dataFiltered, dataThisYear => 
-        //                    d3.max(dataThisYear.countries, d => d.life_exp))])
-        .range([height, 0])
-    x
-        .domain([200, d3.max(dataFiltered, dataThisYear => 
-                           d3.max(dataThisYear.countries, d => d.income))])
-        .range([0, width])
-        .base(10)
-    area
-        .domain([0, d3.max(dataFiltered, dataThisYear => 
-                           d3.max(dataThisYear.countries, d => d.population))])
-        .range([70, 3000])
-        let continents = ["europe", "asia", "americas", "africa"]
-    continentColor
-        .domain(continents)
-        .range(d3.schemeCategory10)
-
-
-    // Add scales
-    let yAxisCall= d3.axisLeft(y)
-    chart.append("g")
-        .attr("class", "y-axis")
-        .call(yAxisCall)
-
-    let xAxisCall = d3.axisBottom(x)
-        .ticks(3)
-        .tickValues([400, 4000, 40000])
-        .tickFormat(d => "$" + d)
-    chart.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxisCall)
- 
-
-    // Add legend
-    let legend = chart.append("g")
-        .attr("transform", "translate(" + (width - 10) + "," + (height - 125) + ")")
-    
-    continents.forEach(function(continentCurrent, i){
-        let legendRow = legend.append("g")
-            .attr("transform", "translate(0, " + (i * 20) + ")")
-
-        legendRow.append("rect")
+        row.append("rect")
             .attr("width", 10)
             .attr("height", 10)
-            .attr("fill", continentColor(continentCurrent))
+            .attr("fill", continentColor(continent));
 
-        legendRow.append("text")
+        row.append("text")
             .attr("x", -10)
             .attr("y", 10)
             .attr("text-anchor", "end")
             .style("text-transform", "capitalize")
-            .text(continentCurrent)       
+            .text(continent);
     })
 }
 
-
-function step(){
-    // At the end of our data, loop back
-    time = (time < 214) ? time+1 : 0
-    update(dataFiltered[time]);
+function isNotNaN(num){
+    return !isNaN(parseFloat(num));
 }
 
 
-function update(dataObject){
-    // Create separate transition object for each transition
-    let t = d3.transition().duration(timeInterval)
 
-    let data = dataObject.countries
-    let current_year = dataObject.year
-    current_year_text
-        .text(current_year)
 
+
+// Load data and initialize the chart
+d3.json("data/data.json").then(function(rawData){
     
-    let continent = $("#continent-select").val();
-    
-    if (continent !== "all") {
-        data = data.filter((d) => {
-            return d.continent === continent
+    // Clean up data
+    data = rawData.map(annualData => {
+
+        let year = annualData.year,
+            countries = annualData.countries.filter(country => {
+                return isNotNaN(country.life_exp) && isNotNaN(country.income) && isNotNaN(country.population);
+            });
+
+        return {"year": year,
+                "countries": countries           
+                };
+    })
+    dataLength = data.length;
+
+    // Compute scales
+    let maxLifeExp = 0,
+        maxIncome = 0,
+        maxPopulation = 0,
+        continents = {};
+    data.forEach(annualData => {
+        annualData.countries.forEach(country => {
+            if (country.life_exp > maxLifeExp) maxLifeExp = country.life_exp;
+            if (country.income > maxIncome) maxIncome = country.income;
+            if (country.population > maxPopulation) maxPopulation = country.population;
+            if (!continents[country.continent]) continents[country.continent] = true;
         })
-    }
+    })
+    y.domain([0, maxLifeExp]);
+    x.domain([200, maxIncome]);
+    area.domain([0, maxPopulation]);
+    continentColor.domain(Object.keys(continents));
+
+    // Draw scales
+    chart.append("g")
+        .attr("class", "y-axis")
+        .call(yAxisCall);
+    chart.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxisCall);
+
+    drawLegend(Object.keys(continents));
+  
+    // Initial visualization run
+    updateChart(data[0]);
     
-    $("#year")[0].innerHTML = +(time + 1800)
-    $("#date-slider").slider("value", +(time + 1800))
+})
+
+function step(){
+    // At the end of data, loop back
+    time = (time < dataLength - 1) ? time + 1 : 0;
+    updateChart(data[time]);
+}
+
+function updateChart(annualData){
+        // Selected continent value
+    let continent = $("#continent-select").val(),
+        // Create individual transition object for each transition
+        t = d3.transition().duration(timeInterval);
+    
+    // Update UI elements
+    currentYearLabel.text(annualData.year);
+    $("#year")[0].innerHTML = +(time + 1800);
+    $("#date-slider").slider("value", +(time + 1800));
+
 
     // JOIN new data with old elements 
     let circles = chart.selectAll("circle")
-        .data(data, function(d){
-            // Data JOIN tracks the items based on the country value instead of the item index in the array
-            return d.country
-        })
+        .data(annualData.countries, function(d){
+            // Data JOIN tracks the items based on the country value instead of the array item index
+            return d.country;
+        });
 
     // EXIT old elements
     circles.exit()
         .transition(t)
-        .attr("r", 0)
-        .remove()
+            .attr("opacity", 0)
+            .attr("r", 0)
+            .remove();
     
     // UPDATE old elements present in new data
     circles
         .transition(t)
-        .attr("cy", d => y(d.life_exp))
-        .attr("cx", d => x(d.income))
-        .attr("r", d => Math.sqrt(area(d.population)) / Math.PI)
+            .attr("cy", d => y(d.life_exp))
+            .attr("cx", d => x(d.income))
+            .attr("r", d => Math.sqrt(area(d.population)) / Math.PI)
+            .attr("opacity", d => continent === "all" ? 1 : 
+                                    d.continent === continent ? 1 : 0);
 
     // ENTER new elements presented in new data 
     circles.enter()
@@ -250,11 +247,12 @@ function update(dataObject){
         .attr("cy", d => y(d.life_exp))
         .attr("cx", d => x(d.income))
         .attr("r", 0)
+        .attr("opacity", 0)
         .on("mouseover", tip.show)
         .on("mouseout", tip.hide)
-        // AND UPDATE old elements present in new data
-        // in this case, circles represents UPDATE selection
         .merge(circles)
         .transition(t)
             .attr("r", d => Math.sqrt(area(d.population)/ Math.PI))  
+            .attr("opacity", d => continent === "all" ? 1 : 
+                                    d.continent === continent ? 1 : 0);
 }
